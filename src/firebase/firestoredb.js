ce4b4bb7ce31ab0aa332d8d3f158efb235f59cf1
danniolsen <danni.r.olsen@gmail.com>
2020-12-15 19:48:59 +0100

    removing firebase dublicats

--- src/firebase/firestoredb.js
+++ src/firebase/firestoredb.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("../../carbuilderServiceAccount.json");
 
-const addDataToFirestore = async (collection, doc, data, db) => {
+const addDataToFirestore = async (collection, data, db) => {
   const docRef = db.collection(collection).doc(data.name);
 
   return await docRef.set({
